3782f16f6a6450aa074fd2a2f39b773f968cb78c
Xico <xico.reasonzx@gmail.com>
2019-11-28 16:52:35 +0000

    PNGs

--- src/Gamestate.java
+++ src/Gamestate.java
@@ -11,11 +11,14 @@ import org.newdawn.slick.state.StateBasedGame;
 public class Gamestate extends BasicGameState{
 
 	 static GameLogic L;
-	 static Bomber Elemento;
+	 static Bomber player;
+	 static Wall walls;
+	 static Bomb bombs;
+	 private ArrayList<Element> elements;
 	
 	public void init(GameContainer arg0, StateBasedGame arg1) throws SlickException {
-		 Elemento=new Bomber(1,1);
-	     L=new GameLogic(Elemento);
+		 player=new Bomber(1,1);
+	     L=new GameLogic(player);
 	}
 
 	public void update(GameContainer container, StateBasedGame sbg, int arg2) throws SlickException {
@@ -39,9 +42,24 @@ public class Gamestate extends BasicGameState{
 
 	public void render(GameContainer arg0, StateBasedGame arg1, Graphics g) throws SlickException {
 		g.drawString("Game state", 50, 50);
-		g.drawString(Elemento.toString(),100,100);
-		//bomber_im.draw(bomber.getX(),bomber.getY());
+		g.drawString(player.toString(),100,100);
+		g.drawImage(player.Get_Image(), 32*player.getX(), 32*player.getY());
 		
+		for(int x = 0 ; x < L.m.Get_RightBound() ; x++) {
+			for(int y = 0 ; y < L.m.Get_BotBound() ; y++) {
+					elements=L.m.Get_List_Elements(x, y);
+					for(int i = 0; i < elements.size(); i++) 
+						if(elements.get(i) instanceof Bomb ) {
+							g.drawImage(bombs.Get_Image(), 32*bombs.getX(), 32*bombs.getY());
+						}
+						else if(elements.get(i) instanceof Bomber ) {
+							g.drawImage(player.Get_Image(), 32*player.getX(), 32*player.getY());
+						}
+						else if(elements.get(i) instanceof Wall ) {
+							g.drawImage(walls.Get_Image(), 32*walls.getX(), 32*walls.getY());
+						}
+					}
+		}
 	}
 
 	public int getID() {
