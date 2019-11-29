76f3e4e5f2a9af4da3c25ea1a1696e74e5ddd46e
gomes <gomes@LAPTOP-B4MEUH7R.lan>
2019-11-29 16:59:56 +0000

    Walking in all directions

--- src/Bomber.java
+++ src/Bomber.java
@@ -8,11 +8,21 @@ public class Bomber extends Element{
 	protected boolean bomb_cd;
 	protected boolean Walking_cd;
 	private int cooldown = 1000; //milliseconds
-	protected int move_res = 20;
+	protected int move_res = 10;
 	protected int progression_count;
-	Image StopFw = new Image("sprites/parado.png");
-	Image WalkFw1 = new Image("sprites/andar1.png");
-	Image WalkFw2 = new Image("sprites/andar2.png");
+	protected int direction;	//0-down 1-left 2-up 3-right
+	Image StopDown = new Image("sprites/StopDown.png");
+	Image Down1 = new Image("sprites/Down1.png");
+	Image Down2 = new Image("sprites/Down2.png");
+	Image StopUp = new Image("sprites/StopUp.png");
+	Image Up1 = new Image("sprites/Up1.png");
+	Image Up2 = new Image("sprites/Up2.png");
+	Image StopLeft = new Image("sprites/StopLeft.png");
+	Image Left1 = new Image("sprites/Left1.png");
+	Image Left2 = new Image("sprites/Left2.png");
+	Image StopRight = new Image("sprites/StopRight.png");
+	Image Right1 = new Image("sprites/Right1.png");
+	Image Right2 = new Image("sprites/Right2.png");
 	
 	
 	Bomber(int x,int y) throws SlickException{
@@ -20,29 +30,34 @@ public class Bomber extends Element{
 		Coord=tmp;
 		Solid=true;
 
-		Set_Image(StopFw);
+		Set_Image(StopDown);
 
 		bomb_cd=false;
 		Walking_cd=false;
+		direction=0;
 	}
 	
 	public void MoveUp() {
 		Coord.MoveUp();
-		Start_Walking();
+		direction=2;
+		Start_Walking();	
 	}
 	
 	public void MoveRight() {
 		Coord.MoveRight();
+		direction=3;
 		Start_Walking();
 	}
 	
 	public void MoveLeft() {
 		Coord.MoveLeft();
+		direction=1;
 		Start_Walking();
 	}
 	
 	public void MoveDown() {
 		Coord.MoveDown();
+		direction=0;
 		Start_Walking();
 	}
 	
@@ -91,7 +106,14 @@ public class Bomber extends Element{
 			if(feet) {
 				feet=!feet;
 				try {
-					Set_Image(WalkFw1);
+					if(direction==0)
+						Set_Image(Down1);
+					if(direction==1)
+						Set_Image(Left1);
+					if(direction==2)
+						Set_Image(Up1);
+					if(direction==3)
+						Set_Image(Right1);
 				} catch (SlickException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -100,7 +122,14 @@ public class Bomber extends Element{
 			else {
 				feet=!feet;
 				try {
-					Set_Image(WalkFw2);
+					if(direction==0)
+						Set_Image(Down2);
+					if(direction==1)
+						Set_Image(Left2);
+					if(direction==2)
+						Set_Image(Up2);
+					if(direction==3)
+						Set_Image(Right2);
 				} catch (SlickException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -109,6 +138,19 @@ public class Bomber extends Element{
 			
 			if(progression_count==move_res){
 				Walking_cd=false;
+				try {
+					if(direction==0)
+						Set_Image(StopDown);
+					if(direction==1)
+						Set_Image(StopLeft);
+					if(direction==2)
+						Set_Image(StopUp);
+					if(direction==3)
+						Set_Image(StopRight);
+				} catch (SlickException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 				this.cancel();
 			}
 		}
